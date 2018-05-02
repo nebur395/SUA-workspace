e95409acc3f38cfabb650304a720dbb4ad98b6ae
Jorge <jormonm5@inf.upv.es>
2018-05-02 17:37:13 +0200

    Constructor analyzer

--- SUA2018.MAPEK/src/es/upv/dsic/mitss/sua/smartcarsae/mapek/impl/Analyzer.java
+++ SUA2018.MAPEK/src/es/upv/dsic/mitss/sua/smartcarsae/mapek/impl/Analyzer.java
@@ -2,13 +2,24 @@ package es.upv.dsic.mitss.sua.smartcarsae.mapek.impl;
 
 import es.upv.dsic.mitss.sua.smartcarsae.mapek.interfaces.IAnalyzer;
 import es.upv.dsic.mitss.sua.smartcarsae.mapek.interfaces.IEvent;
+import es.upv.dsic.mitss.sua.smartcarsae.mapek.interfaces.IPlannifier;
 
 public class Analyzer implements IAnalyzer {
 
+	private IPlannifier plannifier;
+	
+	public Analyzer() {
+		
+	}
+	
 	@Override
 	public void notifyEvent(IEvent event) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public void setPlannifier(IPlannifier plannifier) {
+		this.plannifier = plannifier;
 	}
 
 }
