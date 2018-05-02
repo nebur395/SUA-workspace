f873120e737e970a9a76e8ebffb60682e1cd0182
nebur395 <nebur395@hotmail.com>
2018-05-02 18:16:09 +0200

    Added setExecutor and setKnowledge to plannifier

--- SUA2018.MAPEK/src/es/upv/dsic/mitss/sua/smartcarsae/mapek/impl/Plannifier.java
+++ SUA2018.MAPEK/src/es/upv/dsic/mitss/sua/smartcarsae/mapek/impl/Plannifier.java
@@ -1,14 +1,33 @@
 package es.upv.dsic.mitss.sua.smartcarsae.mapek.impl;
 
+import java.util.List;
+import es.upv.pros.tatami.autonomic.adaptation.framework.systemAPI.componentConfigurator.interfaces.IAdaptiveReadyComponentConfigurator;
 import es.upv.dsic.mitss.sua.smartcarsae.mapek.interfaces.IPlannifier;
+import es.upv.dsic.mitss.sua.smartcarsae.mapek.interfaces.IKnowledge;
+import es.upv.dsic.mitss.sua.smartcarsae.mapek.interfaces.IExecutor;
 import es.upv.dsic.mitss.sua.smartcarsae.mapek.interfaces.ISystemConfiguration;
 
 public class Plannifier implements IPlannifier {
 
+	private IKnowledge knowledge;
+	private IExecutor executor;
+	
 	@Override
 	public void plan(ISystemConfiguration theSystemConfiguration) {
-		// TODO Auto-generated method stub
+		List<IAdaptiveReadyComponentConfigurator> newActiveComponents = theSystemConfiguration.getAdaptiveReadyComponentList();
+		List<IAdaptiveReadyComponentConfigurator> actualActiveComponents = this.knowledge.getCurrentSystemConfiguration().getAdaptiveReadyComponentList();
 		
+		for (IAdaptiveReadyComponentConfigurator component: actualActiveComponents) {
+			
+		}
+	}
+	
+	public void setExecutor(IExecutor executor) {
+		this.executor = executor;
+	}
+	
+	public void setKnowledge(IKnowledge knowledge) {
+		this.knowledge = knowledge;
 	}
 
 }
