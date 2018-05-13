4fc587bfbc7eff393799fa7f6dab3402336c85cc
nebur395 <nebur395@hotmail.com>
2018-05-13 12:50:42 +0200

    Added all logic workflow to the plannifier

--- SUA2018.MAPEK/src/es/upv/dsic/mitss/sua/smartcarsae/mapek/impl/Plannifier.java
+++ SUA2018.MAPEK/src/es/upv/dsic/mitss/sua/smartcarsae/mapek/impl/Plannifier.java
@@ -5,6 +5,8 @@ import java.util.List;
 import es.upv.pros.tatami.autonomic.adaptation.framework.systemAPI.componentConfigurator.interfaces.IAdaptiveReadyComponentConfigurator;
 import es.upv.dsic.mitss.sua.smartcarsae.mapek.interfaces.IPlannifier;
 import es.upv.dsic.mitss.sua.smartcarsae.mapek.interfaces.IKnowledge;
+import es.upv.dsic.mitss.sua.smartcarsae.mapek.interfaces.IAdaptationAction;
+import es.upv.dsic.mitss.sua.smartcarsae.mapek.interfaces.IAdaptationPlan;
 import es.upv.dsic.mitss.sua.smartcarsae.mapek.interfaces.IExecutor;
 import es.upv.dsic.mitss.sua.smartcarsae.mapek.interfaces.ISystemConfiguration;
 
@@ -16,25 +18,53 @@ public class Plannifier implements IPlannifier {
 	@Override
 	public void plan(ISystemConfiguration theSystemConfiguration) {
 		List<IAdaptiveReadyComponentConfigurator> newActiveComponents = theSystemConfiguration.getAdaptiveReadyComponentList();
-		List<IAdaptiveReadyComponentConfigurator> actualActiveComponents = this.knowledge.getCurrentSystemConfiguration().getAdaptiveReadyComponentList();
-	
+		List<IAdaptiveReadyComponentConfigurator> currentActiveComponents = this.knowledge.getCurrentSystemConfiguration().getAdaptiveReadyComponentList();
+		List<IAdaptationAction> newAdaptationActionsList = new LinkedList<IAdaptationAction>();
 		
-		for (IAdaptiveReadyComponentConfigurator actualComponent: actualActiveComponents) {
+		/*
+		 * First iteration: It checks which of the current components have to be undeployed since they are
+		 * not in the new system configuration.
+		 */
+		for (IAdaptiveReadyComponentConfigurator currentComponent: currentActiveComponents) {
 			boolean componentExists = false;
-			
-			// Check if the actual component is in the new configuration plan
+			// Check if the current component is in the new configuration plan
 			for (IAdaptiveReadyComponentConfigurator newComponent: newActiveComponents) {
-				if (actualComponent.getId().equals(newComponent.getId())) {
+				if (currentComponent.getId().equals(newComponent.getId())) {
 					componentExists = true;
 					//newComponent.
 					break;
 				}
 			}
 			
-			if (componentExists) {
-				// agregar a la lista el componente que se activa
+			if (!componentExists) {
+				IAdaptationAction newAdaptationAction = new AdaptationAction(currentComponent, EAdaptationAction.undeploy);
+				newAdaptationActionsList.add(newAdaptationAction);
 			}
 		}
+		
+		/*
+		 * Second iteration: It checks which of the new components have to be deployed since they are
+		 * not in the current system configuration.
+		 */
+		for (IAdaptiveReadyComponentConfigurator newComponent: newActiveComponents) {
+			boolean componentDoesNotExist = true;
+			// Check if the new component is in the current configuration plan
+			for (IAdaptiveReadyComponentConfigurator currentComponent: currentActiveComponents) {
+				if (currentComponent.getId().equals(newComponent.getId())) {
+					componentDoesNotExist = false;
+					//newComponent.
+					break;
+				}
+			}
+			
+			if (componentDoesNotExist) {
+				IAdaptationAction newAdaptationAction = new AdaptationAction(newComponent, EAdaptationAction.deploy);
+				newAdaptationActionsList.add(newAdaptationAction);
+			}
+		}
+		
+		IAdaptationPlan newAdaptationPlan = new AdaptationPlan(newAdaptationActionsList);
+		this.executor.execute(newAdaptationPlan);
 	}
 	
 	public void setExecutor(IExecutor executor) {
