7e96fff48463fa7e8a4ca5442a21c450dd9939d0
nebur395 <nebur395@hotmail.com>
2018-05-13 12:12:52 +0200

    Refactored adaptationPlan and adaptationAction due to existing interfaces

--- SUA2018.MAPEK/src/es/upv/dsic/mitss/sua/smartcarsae/mapek/impl/AdaptationActions.java
+++
@@ -1,6 +0,0 @@
-package es.upv.dsic.mitss.sua.smartcarsae.mapek.impl;
-
-public enum AdaptationActions {
-	deploy,
-	undeploy
-}
--- SUA2018.MAPEK/src/es/upv/dsic/mitss/sua/smartcarsae/mapek/impl/Plannifier.java
+++ SUA2018.MAPEK/src/es/upv/dsic/mitss/sua/smartcarsae/mapek/impl/Plannifier.java
@@ -1,5 +1,6 @@
 package es.upv.dsic.mitss.sua.smartcarsae.mapek.impl;
 
+import java.util.LinkedList;
 import java.util.List;
 import es.upv.pros.tatami.autonomic.adaptation.framework.systemAPI.componentConfigurator.interfaces.IAdaptiveReadyComponentConfigurator;
 import es.upv.dsic.mitss.sua.smartcarsae.mapek.interfaces.IPlannifier;
@@ -16,9 +17,23 @@ public class Plannifier implements IPlannifier {
 	public void plan(ISystemConfiguration theSystemConfiguration) {
 		List<IAdaptiveReadyComponentConfigurator> newActiveComponents = theSystemConfiguration.getAdaptiveReadyComponentList();
 		List<IAdaptiveReadyComponentConfigurator> actualActiveComponents = this.knowledge.getCurrentSystemConfiguration().getAdaptiveReadyComponentList();
+	
 		
-		for (IAdaptiveReadyComponentConfigurator component: actualActiveComponents) {
+		for (IAdaptiveReadyComponentConfigurator actualComponent: actualActiveComponents) {
+			boolean componentExists = false;
 			
+			// Check if the actual component is in the new configuration plan
+			for (IAdaptiveReadyComponentConfigurator newComponent: newActiveComponents) {
+				if (actualComponent.getId().equals(newComponent.getId())) {
+					componentExists = true;
+					//newComponent.
+					break;
+				}
+			}
+			
+			if (componentExists) {
+				// agregar a la lista el componente que se activa
+			}
 		}
 	}
 	
